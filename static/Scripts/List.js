11e1f549ec6cd403130a3486dd92e70de0f3ff3b
Karthik Kumar Viswanathan <kviswanathan@zynga.com>
2012-10-09 18:57:35 +0530

    Fixes

--- static/Scripts/List.js
+++ static/Scripts/List.js
@@ -1,6 +1,6 @@
 var Servers = [
-	"ws://192.168.2.5:8080",
-	"ws://192.168.2.6:8080",
+	"ws://karthik.jit.suz:8080",
+	"ws://sonali.jit.suz:8080",
 ];
 
 var UserServers = Servers;
--- turntable.js
+++ turntable.js
@@ -5,36 +5,35 @@ var port = 8080;
 var webrootrel = "static";
 //End Server Settings.
 
-//Begin Common Code.
-var users = {};
-var connections = [];
-var userCommands = [];
+//Begin Command Dispatcher for Server.
+var commandDispatchers = {};
 
-function handleConnectionSend(connection, message) {
-	connection.sendUTF(JSON.stringify(message));
+function registerCallback(command, callback) {
+	commandDispatchers[command] = callback;
 }
 
-function handleConnectionOpen(connection) {
-    connections.push(connection);
-    console.log(connection.remoteAddress + " Connected - Protocol Version " + connection.websocketVersion);
-    handleConnectionSend(connection, { command: "idyou", data: {} });
+function unregisterCallback(command) {
+	registerCallback(command, null);
 }
 
-function handleConnectionClose(connection) {
-    console.log(connection.remoteAddress + " Disconnected");
-    var index;
-    index = connections.indexOf(connection);
-    if (index !== -1) { // remove the connection from the pool
-        connections.splice(index, 1);
-    }
-    for(var userid in users) {
-	if(users[userid] === connection) {
-		console.log("Removing User: "+userid+" ....");
-		delete users[userid];
-		break;
+function dispatchCommand(connection, command, data) {
+	var handler = null;
+	if(commandDispatchers[command] && typeof(commandDispatchers[command]) === 'function') {
+		handler = commandDispatchers[command];
+		if(handler) {
+			handler(connection, command, data);
+		} else {
+			console.log("Recieved but not able to Dispatch Message: %o %o", command, data);
+		}
 	}
-    }
 }
+//End Command Dispatcher for Server.
+
+//Begin Commands.
+
+//Begin Command Variables.
+var users = {};
+//End Command Variables.
 
 function handleIdMe(connection, command, data) {
 	var userid = data;
@@ -58,23 +57,61 @@ function handleWhoThere(connection, command, data) {
 	handleConnectionSend(connection, {command: "there", data: usersCurrent});
 }
 
-var commandDispatchers = { "whothere": handleWhoThere, "idme": handleIdMe };
-function dispatchCommand(connection, command, data) {
-	var handler = null;
-	if(commandDispatchers[command] && typeof(commandDispatchers[command]) === 'function') {
-		handler = commandDispatchers[command];
-		if(handler) {
-			handler(connection, command, data);
-		} else {
-			console.log("Recieved but not able to Dispatch Message: %o %o", command, data);
-		}
+function handleUserOpen(connection, command, data) {
+    handleConnectionSend(connection, { command: "idyou", data: {} });
+}
+
+function handleUserClose(connection, command, data) {
+    for(var userid in users) {
+	console.log("User: "+userid);
+	if(users[userid] == connection) {
+		console.log("Removing User: "+userid+" ....");
+		delete users[userid];
+		break;
 	}
+    }
+}
+
+//End Commands.
+
+//Begin Registration of Commands.
+registerCallback("whothere", handleWhoThere);
+registerCallback("idme", handleIdMe);
+registerCallback("Open", handleUserOpen);
+registerCallback("Close", handleUserClose);
+//End Registration of Commands.
+
+//Begin Common Server Side Variables.
+var connections = [];
+//End Common Server Side Variables.
+
+//Begin Common Server Side Code.
+function handleConnectionSend(connection, message) {
+	connection.sendUTF(JSON.stringify(message));
+}
+
+function handleConnectionOpen(connection) {
+    console.log(connection.remoteAddress + " Connected - Protocol Version " + connection.websocketVersion);
+    connections.push(connection);
+    dispatchCommand(connection, "Open", null);
+}
+
+function handleConnectionClose(connection) {
+    var index;
+    dispatchCommand(connection, "Close", null);
+    index = connections.indexOf(connection);
+    if (index !== -1) { // remove the connection from the pool
+    	console.log(connection.remoteAddress + " Disconnected");
+        connections.splice(index, 1);
+    } else {
+    	console.log(connection.remoteAddress + " Not Found");
+    }
 }
 
 function handleConnectionMessage(connection, message) {
     if (message.type !== 'utf8')
 	return;
-    try { 
+    try {
         var parsedMessage = JSON.parse(message.utf8Data);
 	if(!parsedMessage.command) {
 		dispatchCommand(connection, parsedMessage, parsedMessage);
@@ -85,22 +122,23 @@ function handleConnectionMessage(connection, message) {
         // do nothing if there's an error.
     }
 }
-//End Common Code.
+
+//End Common Server Side Code.
 
 //Begin HTTP Server Code.
 var express = require('express');
 if (!express) {
-console.log("Unable to Get Express.");
-process.exit(-1);
+	console.log("Unable to Get Express.");
+	process.exit(-1);
 }
 
 var app = express.createServer();
 var webroot = __dirname + "/" + webrootrel;
 
 app.configure(function() {
-app.use(express.static(webroot));
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
+	app.use(express.static(webroot));
+	app.set('views', __dirname);
+	app.set('view engine', 'ejs');
 });
 
 app.get('/', function(request, response) {
@@ -115,7 +153,6 @@ app.on('error', function(e) {
         }, 5000);
     }
 });
-
 app.listen(port);
 //End HTTP Server Code.
 
@@ -139,7 +176,7 @@ wsServer.on('request', function(request) {
     //Handle open connection
     handleConnectionOpen(connection);
     //Handle closed connection
-    connection.on('close', function() {
+    connection.on('close', function(connection, closeReason, description) {
         handleConnectionClose(connection);
     });
     //Handle incoming messages on connection
@@ -147,5 +184,22 @@ wsServer.on('request', function(request) {
         handleConnectionMessage(connection, message);
     });
 });
-
 //End WebSocket Server Code.
+
+//Begin Process Handling Code.
+function handleExit() {
+	console.log("");
+	console.log("Exiting .... ");
+	wsServer.shutDown();
+	app.close();	
+	console.log("Process was Exited.");
+	process.exit(0);
+}
+
+process.on('SIGINT', handleExit);
+
+//Handle Uncaught Exceptions.
+process.on('uncaughtException', function (err) {
+  console.log('Caught exception: ' + err);
+});
+//End Process Handling Code.
