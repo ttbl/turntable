7db7b7fd2b4556c84ecb048ba3fff604b618e93e
Karthik Kumar Viswanathan <kviswanathan@zynga.com>
2012-10-12 19:27:53 +0530

    Fixes to Deployment

--- static/Scripts/List.js
+++ static/Scripts/List.js
@@ -1,8 +1,8 @@
 var Servers = [
-	"ws://turntable001.jit.su","ws://turntable002.jit.su","ws://turntable003.jit.su","ws://turntable004.jit.su","ws://turntable005.jit.su","ws://turntable006.jit.su","ws://turntable007.jit.su","ws://turntable008.jit.su","ws://turntable009.jit.su","ws://turntable010.jit.su","ws://turntable011.jit.su","ws://turntable012.jit.su","ws://turntable013.jit.su","ws://turntable014.jit.su","ws://turntable015.jit.su","ws://turntable016.jit.su","ws://turntable017.jit.su","ws://turntable018.jit.su","ws://turntable019.jit.su","ws://turntable020.jit.su","ws://turntable021.jit.su","ws://turntable022.jit.su","ws://turntable023.jit.su","ws://turntable024.jit.su","ws://turntable025.jit.su","ws://turntable026.jit.su","ws://turntable027.jit.su","ws://turntable028.jit.su","ws://turntable029.jit.su","ws://turntable030.jit.su","ws://turntable031.jit.su","ws://turntable032.jit.su","ws://turntable033.jit.su","ws://turntable034.jit.su","ws://turntable035.jit.su",
+	"ws://localhost:8080",
 ];
 
-var SelfId = "ws://turntable001.jit.su";
+var SelfId = "ws://localhost:8080";
 
 var UserServers = Servers;
 var GameServers = Servers;
